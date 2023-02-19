49d7c925186643d94d6e8bc13695782f5e83e2a5
Vitalina Krasnoplakhtich <krasnoplakhtich.vitalina@gmail.com>
2023-02-19 17:39:40 +0200

    Create second task

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,17 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsList = document.querySelector('#ingredients');
+
+const ingredientsArray = ingredients.map((ingredient) => { 
+const itemOfList = document.createElement("li");
+itemOfList.textContent = ingredient;
+itemOfList.classList.add("item");
+
+return itemOfList;
+});
+
+
+ingredientsList.append(...ingredientsArray);
+console.log(ingredientsList);
